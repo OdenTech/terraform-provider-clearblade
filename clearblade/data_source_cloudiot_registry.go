879f42d621fd1f0a9fbd868a6530e29091920bee
mikoaro <mikoarodeveloper@gmail.com>
2023-05-24 11:01:25 -0500

    Implement data source schema for device registries

--- clearblade/data_source_cloudiot_registry.go
+++ clearblade/data_source_cloudiot_registry.go
@@ -15,36 +15,60 @@ var (
 	_ datasource.DataSourceWithConfigure = &deviceRegistriesDataSource{}
 )
 
+
 // deviceRegistriesDataSourceModel maps the data source schema data.
 type deviceRegistriesDataSourceModel struct {
-	DeviceRegistries []deviceRegistriesModel `tfsdk:"deviceRegistries"`
+	DeviceRegistries []deviceRegistriesModel `tfsdk:"device_registries"`
 }
 
 // deviceRegistriesModel maps deviceRegistry schema data.
 type deviceRegistriesModel struct {
-	Name                     types.String                   `tfsdk:"name"`
-	EventNotificationConfigs []eventNotificationConfigModel `tfsdk:"eventNotificationConfigs"`
-	StateNotificationConfig  stateNotificationConfigModel   `tfsdk:"stateNotificationConfig"`
-	MqttConfig               mqttConfigModel                `tfsdk:"mqttConfig"`
-	HttpConfig               httpConfigModel                `tfsdk:"httpConfig"`
-	LogLevel                 types.String                   `tfsdk:"logLevel"`
+	ID          				types.String               		`tfsdk:"id"`
+	Name                     	types.String                   	`tfsdk:"name"`
+	EventNotificationConfigs 	[]eventNotificationConfigModel 	`tfsdk:"event_notification_configs"`
+	StateNotificationConfig  	stateNotificationConfigModel   	`tfsdk:"state_notification_config"`
+	HttpConfig               	httpConfigModel                	`tfsdk:"http_config"`
+	MqttConfig               	mqttConfigModel                	`tfsdk:"mqtt_config"`
+	LogLevel                 	types.String                   	`tfsdk:"log_level"`
+	Credentials 				[]credentialsModel 				`tfsdk:"credentials"`
 }
 
 type eventNotificationConfigModel struct {
-	SubFolderMatches types.String `tfsdk:"subfolderMatches"`
-	PubsubTopicName  types.String `tfsdk:"pubsubTopicName"`
+	SubFolderMatches types.String `tfsdk:"subfolder_matches"`
+	PubsubTopicName  types.String `tfsdk:"pubsub_topic_name"`
 }
 
 type stateNotificationConfigModel struct {
-	PubsubTopicName types.String `tfsdk:"pubsubTopicName"`
-}
-
-type mqttConfigModel struct {
-	MqttEnabledState types.String `tfsdk:"mqttEnabledState"`
+	PubsubTopicName types.String `tfsdk:"pubsub_topic_name"`
 }
 
 type httpConfigModel struct {
-	HttpEnabledState types.String `tfsdk:"httpEnabledState"`
+	HttpEnabledState types.String `tfsdk:"http_enabled_state"`
+}
+type mqttConfigModel struct {
+	MqttEnabledState types.String `tfsdk:"mqtt_enabled_state"`
+}
+type credentialsModel struct {
+	PublicKeyCertificate 	publicKeyCertificateModel `tfsdk:"public_key_certificate"`
+}
+
+type publicKeyCertificateModel struct {
+	Format 			types.String 		`tfsdk:"format"`
+	Certificate 	types.String 		`tfsdk:"certificate"`
+	X509Details 	x509DetailsModel 	`tfsdk:"x509_details"`
+}
+
+type x509DetailsModel struct {
+	X509CertificateDetail x509CertificateDetailModel `tfsdk:"X509_certificate_detail"`
+}
+
+type x509CertificateDetailModel struct {
+	Issuer 				types.String `tfsdk:"issuer"`
+	Subject 			types.String `tfsdk:"subject"`
+	StartTime 			types.String `tfsdk:"start_time"`
+	ExpiryTime 			types.String `tfsdk:"expiry_time"`
+	SignatureAlgorithm 	types.String `tfsdk:"signature_algorithm"`
+	PublicKeyType 		types.String `tfsdk:"public_key_type"`
 }
 
 func NewDeviceRegistriesDataSource() datasource.DataSource {
@@ -69,7 +93,136 @@ func (d *deviceRegistriesDataSource) Configure(_ context.Context, req datasource
 }
 
 func (d *deviceRegistriesDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-    resp.Schema = schema.Schema{}
+    resp.Schema = schema.Schema{
+		Description: "List of device registries in a project.",
+		Attributes: map[string]schema.Attribute{
+			"device_registries": schema.ListNestedAttribute{
+				Computed: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Computed: true,
+						},
+						"name": schema.StringAttribute{
+							Computed: true,
+						},
+						"event_notification_configs": schema.ListNestedAttribute{
+							Computed: true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"event_notification_config": schema.SingleNestedAttribute{
+										Required:    true,
+										Description: "The configuration for forwarding telemetry events.",
+										Attributes: map[string]schema.Attribute{
+											"subfolder_matches": schema.StringAttribute{
+												Description: "This field is used only for telemetry events; subfolders are not supported for state changes.",
+												Computed:    true,
+											},
+											"pubsub_topic_name": schema.StringAttribute{
+												Description: "A Cloud Pub/Sub topic name. For example, projects/myProject/topics/deviceEvents.",
+												Computed:    true,
+											},
+										},
+									},
+								},
+							},
+						},
+						"state_notification_config": schema.SingleNestedAttribute{
+							Required:    true,
+							Description: "The configuration for notification of new states received from the device.",
+							Attributes: map[string]schema.Attribute{
+								"pubsub_topic_name": schema.StringAttribute{
+									Description: "A Cloud Pub/Sub topic name. For example, projects/myProject/topics/deviceEvents.",
+									Computed:    true,
+								},
+							},
+						},
+						"http_config": schema.SingleNestedAttribute{
+							Required:    true,
+							Description: "The configuration of the HTTP bridge for a device registry.",
+							Attributes: map[string]schema.Attribute{
+								"http_enabled_state": schema.StringAttribute{
+									Description: "If enabled, allows devices to use DeviceService via the HTTP protocol. Otherwise, any requests to DeviceService will fail for this registry.",
+									Computed:    true,
+								},
+							},
+						},
+						"mqtt_config": schema.SingleNestedAttribute{
+							Required:    true,
+							Description: "The configuration of MQTT for a device registry.",
+							Attributes: map[string]schema.Attribute{
+								"mqtt_enabled_state": schema.StringAttribute{
+									Description: "If enabled, allows connections using the MQTT protocol. Otherwise, MQTT connections to this registry will fail.",
+									Computed:    true,
+								},
+							},
+						},
+						"log_level": schema.StringAttribute{
+							Description: "The logging verbosity for device activity. Specifies which events should be written to logs.",
+							Computed: true,
+						},
+						"credentials": schema.ListNestedAttribute{
+							Computed: true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"credential": schema.SingleNestedAttribute{
+										Required:    true,
+										Description: "A server-stored registry credential used to validate device credentials.",
+										Attributes: map[string]schema.Attribute{
+											"public_key_certificate": schema.SingleNestedAttribute{
+												Required:    true,
+												Description: "A public key certificate format and data.",
+												Attributes: map[string]schema.Attribute{
+													"format": schema.StringAttribute{
+														Description: "The certificate format.",
+														Computed:    true,
+													},
+													"certificate": schema.StringAttribute{
+														Description: "The certificate data.",
+														Computed:    true,
+													},
+													"X509_certificate_detail": schema.SingleNestedAttribute{
+														Required:    true,
+														Description: "The certificate details. Used only for X.509 certificates.",
+														Attributes: map[string]schema.Attribute{
+															"issuer": schema.StringAttribute{
+																Description: "The entity that signed the certificate.",
+																Computed:    true,
+															},
+															"subject": schema.StringAttribute{
+																Description: "The entity the certificate and public key belong to.",
+																Computed:    true,
+															},
+															"start_time": schema.StringAttribute{
+																Description: "The time the certificate becomes valid.",
+																Computed:    true,
+															},
+															"expiry_time": schema.StringAttribute{
+																Description: "The time the certificate becomes invalid.",
+																Computed:    true,
+															},
+															"signature_algorithm": schema.StringAttribute{
+																Description: "The algorithm used to sign the certificate.",
+																Computed:    true,
+															},
+															"public_key_type": schema.StringAttribute{
+																Description: "The type of public key in the certificate.",
+																Computed:    true,
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 }
 
 func (d *deviceRegistriesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
