ab1842c826709f1bf2f6e3e9929d6071c519d514
mikoaro <mikoarodeveloper@gmail.com>
2023-05-31 09:58:56 -0500

    Add device registry resource implementation

---
+++ clearblade/resource_iot_registry.go
@@ -0,0 +1,80 @@
+package clearblade
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/clearblade/go-iot"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ resource.Resource              = &deviceRegistryResource{}
+	_ resource.ResourceWithConfigure = &deviceRegistryResource{}
+	//_ resource.ResourceWithImportState = &deviceRegistryResource{}
+)
+
+type jiraIssueTypeScreenSchemeResourceModel struct {
+	ID   types.String `tfsdk:"id"`
+	Name types.String `tfsdk:"name"`
+}
+
+func NewDeviceRegistryResource() resource.Resource {
+	return &deviceRegistryResource{}
+}
+
+// deviceRegistryResource is the resource implementation.
+type deviceRegistryResource struct {
+	client *iot.Service
+}
+
+// Metadata returns the data source type name.
+func (r *deviceRegistryResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_iot_registry"
+}
+
+// Schema defines the schema for the resource.
+func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{}
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *deviceRegistryResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*iot.Service)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *iot.Service, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	r.client = client
+}
+
+// Create creates the resource and sets the initial Terraform state.
+func (r *deviceRegistryResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	tflog.Debug(ctx, "Creating iot device registry resource")
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *deviceRegistryResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *deviceRegistryResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *deviceRegistryResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
