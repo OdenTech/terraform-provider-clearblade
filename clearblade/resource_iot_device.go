d7303adbfd55db2def0ea605ab76799ead2ca0e9
mikoaro <mikoarodeveloper@gmail.com>
2023-08-02 11:46:32 -0500

    Add device resource

---
+++ clearblade/resource_iot_device.go
@@ -0,0 +1,176 @@
+package clearblade
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/clearblade/go-iot"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ resource.Resource              = &deviceResource{}
+	_ resource.ResourceWithConfigure = &deviceResource{}
+	//_ resource.ResourceWithImportState = &deviceResource{}
+)
+
+type deviceResourceModel struct {
+	Registry    types.String `tfsdk:"registry"`
+	Project     types.String `tfsdk:"project"`
+	Region      types.String `tfsdk:"region"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+	Device      *deviceModel `tfsdk:"device"`
+}
+
+type deviceModel struct {
+	ID types.String `tfsdk:"id"`
+	//Name          types.String        `tfsdk:"name"`
+	GatewayConfig *gatewayConfigModel `tfsdk:"gateway_config"`
+}
+
+type gatewayConfigModel struct {
+	GatewayType       types.String `tfsdk:"gateway_type"`
+	GatewayAuthMethod types.String `tfsdk:"gateway_auth_method"`
+}
+
+func NewDeviceResource() resource.Resource {
+	return &deviceResource{}
+}
+
+// deviceResource is the resource implementation.
+type deviceResource struct {
+	client *iot.Service
+}
+
+// Metadata returns the data source type name.
+func (r *deviceResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_iot_device"
+}
+
+// Schema defines the schema for the resource.
+func (r *deviceResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"registry": schema.StringAttribute{
+				MarkdownDescription: "The name of the device registry where this device should be created.",
+				Required:            true,
+			},
+			"project": schema.StringAttribute{
+				MarkdownDescription: "The id of the project.",
+				Required:            true,
+			},
+			"region": schema.StringAttribute{
+				MarkdownDescription: "The name of the cloud region.",
+				Required:            true,
+			},
+			"last_updated": schema.StringAttribute{
+				Computed: true,
+			},
+			"device": schema.SingleNestedAttribute{
+				MarkdownDescription: "A unique resource.",
+				Required:            true,
+				Attributes: map[string]schema.Attribute{
+					"id": schema.StringAttribute{
+						MarkdownDescription: "A unique name for the device resource.",
+						Required:            true,
+					},
+					"gateway_config": schema.SingleNestedAttribute{
+						MarkdownDescription: "Gateway-related configuration and state.",
+						Optional:            true,
+						Attributes: map[string]schema.Attribute{
+							"gateway_type": schema.StringAttribute{
+								MarkdownDescription: "Indicates whether the device is a gateway.",
+								Optional:            true,
+							},
+							"gateway_auth_method": schema.StringAttribute{
+								MarkdownDescription: "Indicates how to authorize and/or authenticate devices to access the gateway.",
+								Optional:            true,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+// Configure adds the provider configured client to the data source.
+func (r *deviceResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*iot.Service)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *iot.Service, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	r.client = client
+}
+
+// Create creates the resource and sets the initial Terraform state.
+func (r *deviceResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	tflog.Debug(ctx, "Creating iot device resource")
+
+	var plan deviceResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	payload := &iot.Device{}
+	payload.Id = plan.Device.ID.ValueString()
+	if plan.Device.GatewayConfig.GatewayType.ValueString() != "" {
+		payload.GatewayConfig = &iot.GatewayConfig{
+			GatewayType:       plan.Device.GatewayConfig.GatewayType.ValueString(),
+			GatewayAuthMethod: plan.Device.GatewayConfig.GatewayAuthMethod.ValueString(),
+		}
+	}
+
+	parent := fmt.Sprintf("projects/%s/locations/%s/registries/%s", plan.Project.ValueString(), plan.Region.ValueString(), plan.Registry.ValueString())
+
+	// Create new device
+	_, err := r.client.Projects.Locations.Registries.Devices.Create(parent, payload).Do()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating a device",
+			"Could not create a new device, unexpected error: "+err.Error(),
+		)
+		return
+	}
+	tflog.Debug(ctx, "Created a device")
+
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	// Set state to fully populated data
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (r *deviceResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+
+}
+
+// Update updates the resource and sets the updated Terraform state on success.
+func (r *deviceResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+}
+
+// Delete deletes the resource and removes the Terraform state on success.
+func (r *deviceResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
--- clearblade/resource_iot_registry.go
+++ clearblade/resource_iot_registry.go
@@ -9,6 +9,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
@@ -102,16 +105,21 @@ func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequ
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
 				MarkdownDescription: "The identifier of this device registry. For example, myRegistry.",
-				Optional:            true,
+				Required:            true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
 			},
 			"name": schema.StringAttribute{
 				MarkdownDescription: "The resource path name. For example, projects/example-project/locations/us-central1/registries/my-registry.",
 				Optional:            true,
 			},
 			"event_notification_configs": schema.ListNestedAttribute{
+				//"event_notification_configs": schema.ListNestedAttribute{
 				MarkdownDescription: "The configuration for notification of telemetry events received from the device. " +
 					"All telemetry events that were successfully published by the device and acknowledged by Clearblade IoT Core are guaranteed to be delivered to Cloud Pub/Sub.",
-				Required: true,
+				Computed: true,
+				Optional: true,
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"pubsub_topic_name": schema.StringAttribute{
@@ -121,7 +129,7 @@ func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequ
 						"subfolder_matches": schema.StringAttribute{
 							MarkdownDescription: "If the subfolder name matches this string exactly, this configuration will be used. The string must not include the leading '/' character. If empty, all strings are matched. " +
 								"This field is used only for telemetry events; subfolders are not supported for state changes.",
-							Required: true,
+							Optional: true,
 						},
 					},
 				},
@@ -161,7 +169,7 @@ func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequ
 				Required:            true,
 			},
 			"credentials": schema.ListNestedAttribute{
-				Computed: true,
+				Optional: true,
 				NestedObject: schema.NestedAttributeObject{
 					// Attributes: map[string]schema.Attribute{
 					// 	"credential": schema.SingleNestedAttribute{
@@ -174,11 +182,11 @@ func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequ
 							Attributes: map[string]schema.Attribute{
 								"format": schema.StringAttribute{
 									Description: "The certificate format.",
-									Computed:    true,
+									Optional:    true,
 								},
 								"certificate": schema.StringAttribute{
 									Description: "The certificate data.",
-									Computed:    true,
+									Optional:    true,
 								},
 								//
 								"x509_details": schema.SingleNestedAttribute{
@@ -191,27 +199,27 @@ func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequ
 											Attributes: map[string]schema.Attribute{
 												"issuer": schema.StringAttribute{
 													Description: "The entity that signed the certificate.",
-													Computed:    true,
+													Optional:    true,
 												},
 												"subject": schema.StringAttribute{
 													Description: "The entity the certificate and public key belong to.",
-													Computed:    true,
+													Optional:    true,
 												},
 												"start_time": schema.StringAttribute{
 													Description: "The time the certificate becomes valid.",
-													Computed:    true,
+													Optional:    true,
 												},
 												"expiry_time": schema.StringAttribute{
 													Description: "The time the certificate becomes invalid.",
-													Computed:    true,
+													Optional:    true,
 												},
 												"signature_algorithm": schema.StringAttribute{
 													Description: "The algorithm used to sign the certificate.",
-													Computed:    true,
+													Optional:    true,
 												},
 												"public_key_type": schema.StringAttribute{
 													Description: "The type of public key in the certificate.",
-													Computed:    true,
+													Optional:    true,
 												},
 											},
 										},
@@ -224,17 +232,6 @@ func (r *deviceRegistryResource) Schema(_ context.Context, _ resource.SchemaRequ
 					//},//
 				},
 			},
-			// "project": schema.StringAttribute{
-			// 	MarkdownDescription: "The id of the project.",
-			// 	Optional:            true,
-			// },
-			// "region": schema.StringAttribute{
-			// 	MarkdownDescription: "The name of the cloud region.",
-			// 	Optional:            true,
-			// },
-			// "last_updated": schema.StringAttribute{
-			// 	Optional: true,
-			// },
 		},
 	}
 }
@@ -285,14 +282,12 @@ func (r *deviceRegistryResource) Create(ctx context.Context, req resource.Create
 				Certificate: v.PublicKeyCertificate.Certificate.ValueString(),
 				Format:      v.PublicKeyCertificate.Format.ValueString(),
 				X509Details: &iot.X509CertificateDetails{
-					//X509CertificateDetail: &iot. {
 					ExpiryTime:         v.PublicKeyCertificate.X509Details.X509CertificateDetail.ExpiryTime.ValueString(),
 					Issuer:             v.PublicKeyCertificate.X509Details.X509CertificateDetail.Issuer.ValueString(),
 					PublicKeyType:      v.PublicKeyCertificate.X509Details.X509CertificateDetail.PublicKeyType.ValueString(),
 					SignatureAlgorithm: v.PublicKeyCertificate.X509Details.X509CertificateDetail.SignatureAlgorithm.ValueString(),
 					StartTime:          v.PublicKeyCertificate.X509Details.X509CertificateDetail.StartTime.ValueString(),
 					Subject:            v.PublicKeyCertificate.X509Details.X509CertificateDetail.Subject.ValueString(),
-					//},
 				},
 			},
 		})
@@ -321,11 +316,10 @@ func (r *deviceRegistryResource) Create(ctx context.Context, req resource.Create
 		LogLevel:                 plan.LogLevel.ValueString(),
 	}
 
-	//parent := fmt.Sprintf("projects/%s/locations/%s", plan.Project.ValueString(), plan.Region.ValueString())
 	parent := fmt.Sprintf("projects/%s/locations/%s", os.Getenv("CLEARBLADE_PROJECT"), os.Getenv("CLEARBLADE_REGION"))
 
 	// Create new registry
-	_, err := r.client.Projects.Locations.Registries.Create(parent, &createRequestPayload).Do()
+	registry, err := r.client.Projects.Locations.Registries.Create(parent, &createRequestPayload).Do()
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"Error creating a device registry",
@@ -336,11 +330,39 @@ func (r *deviceRegistryResource) Create(ctx context.Context, req resource.Create
 	tflog.Debug(ctx, "Created a device registry")
 
 	// Map response body to schema and populate Computed attribute values
-	//plan.ID = types.StringValue(registry.Id)
-	//plan.Project = types.StringValue(plan.Project.ValueString())
-	//plan.Region = types.StringValue(plan.Region.ValueString())
+	// plan.Credentials = []credentialsModel{}
+	// for _, credential := range registry.Credentials {
+	// 	plan.Credentials = append(plan.Credentials, credentialsModel{
+	// 		PublicKeyCertificate: publicKeyCertificateModel{
+	// 			Format:      types.StringValue(credential.PublicKeyCertificate.Format),
+	// 			Certificate: types.StringValue(credential.PublicKeyCertificate.Certificate),
+	// 			X509Details: x509DetailsModel{
+	// 				X509CertificateDetail: x509CertificateDetailModel{
+	// 					Issuer:             types.StringValue(credential.PublicKeyCertificate.X509Details.Issuer),
+	// 					Subject:            types.StringValue(credential.PublicKeyCertificate.X509Details.Subject),
+	// 					StartTime:          types.StringValue(credential.PublicKeyCertificate.X509Details.StartTime),
+	// 					ExpiryTime:         types.StringValue(credential.PublicKeyCertificate.X509Details.ExpiryTime),
+	// 					SignatureAlgorithm: types.StringValue(credential.PublicKeyCertificate.X509Details.SignatureAlgorithm),
+	// 					PublicKeyType:      types.StringValue(credential.PublicKeyCertificate.X509Details.PublicKeyType),
+	// 				},
+	// 			},
+	// 		},
+	// 	})
+	// }
 
-	//plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+	plan.EventNotificationConfigs = []eventNotificationConfigsModel{}
+	for _, eventNotificationConfig := range registry.EventNotificationConfigs {
+		plan.EventNotificationConfigs = append(plan.EventNotificationConfigs, eventNotificationConfigsModel{
+			PubsubTopicName:  types.StringValue(eventNotificationConfig.PubsubTopicName),
+			SubfolderMatches: types.StringValue(eventNotificationConfig.SubfolderMatches),
+		})
+	}
+
+	plan.HttpConfig.HttpEnabledState = types.StringValue(registry.HttpConfig.HttpEnabledState)
+	plan.MqttConfig.MqttEnabledState = types.StringValue(registry.MqttConfig.MqttEnabledState)
+	plan.StateNotificationConfig.PubsubTopicName = types.StringValue(registry.StateNotificationConfig.PubsubTopicName)
+	plan.LogLevel = types.StringValue(registry.LogLevel)
+	//plan.Name = types.StringValue(registry.Name)
 
 	// Set state to fully populated data
 	diags = resp.State.Set(ctx, plan)
@@ -364,10 +386,6 @@ func (r *deviceRegistryResource) Read(ctx context.Context, req resource.ReadRequ
 		return
 	}
 
-	tflog.Info(ctx, "In Read method: Start STATE logging")
-	ctx = tflog.SetField(ctx, "clearblade_provider_state", state)
-	tflog.Info(ctx, "In Read method: Stop STATE logging")
-
 	// Get refreshed registry value from ClearBlade IoT Core
 	parent := fmt.Sprintf("projects/%s/locations/%s/registries/%s", os.Getenv("CLEARBLADE_PROJECT"), os.Getenv("CLEARBLADE_REGION"), state.ID.ValueString())
 	registry, err := r.client.Projects.Locations.Registries.Get(parent).Do()
@@ -379,13 +397,55 @@ func (r *deviceRegistryResource) Read(ctx context.Context, req resource.ReadRequ
 		return
 	}
 
-	//state.Registry.EventNotificationConfigs = types.StringValue(registry.EventNotificationConfigs)
-	//state.StateNotificationConfig.PubsubTopicName = types.StringValue(registry.StateNotificationConfig.PubsubTopicName)
-	//state.MqttConfig.MqttConfig = types.StringValue(registry.MqttConfig.MqttEnabledState)
-	//state.HttpConfig.HttpConfig = types.StringValue(registry.HttpConfig.HttpEnabledState)
+	// Overwrite items with refreshed state
+	state.Credentials = []credentialsModel{}
+	for _, credential := range registry.Credentials {
+		state.Credentials = append(state.Credentials, credentialsModel{
+			PublicKeyCertificate: publicKeyCertificateModel{
+				Format:      types.StringValue(credential.PublicKeyCertificate.Format),
+				Certificate: types.StringValue(credential.PublicKeyCertificate.Certificate),
+				X509Details: x509DetailsModel{
+					X509CertificateDetail: x509CertificateDetailModel{
+						Issuer:             types.StringValue(credential.PublicKeyCertificate.X509Details.Issuer),
+						Subject:            types.StringValue(credential.PublicKeyCertificate.X509Details.Subject),
+						StartTime:          types.StringValue(credential.PublicKeyCertificate.X509Details.StartTime),
+						ExpiryTime:         types.StringValue(credential.PublicKeyCertificate.X509Details.ExpiryTime),
+						SignatureAlgorithm: types.StringValue(credential.PublicKeyCertificate.X509Details.SignatureAlgorithm),
+						PublicKeyType:      types.StringValue(credential.PublicKeyCertificate.X509Details.PublicKeyType),
+					},
+				},
+			},
+		})
+	}
+
+	state.EventNotificationConfigs = []eventNotificationConfigsModel{}
+	for _, eventNotificationConfig := range registry.EventNotificationConfigs {
+		state.EventNotificationConfigs = append(state.EventNotificationConfigs, eventNotificationConfigsModel{
+			PubsubTopicName:  types.StringValue(eventNotificationConfig.PubsubTopicName),
+			SubfolderMatches: types.StringValue(eventNotificationConfig.SubfolderMatches),
+		})
+	}
+
+	tflog.Info(ctx, "In Read method: Start STATE logging")
+	ctx = tflog.SetField(ctx, "clearblade_provider_state", registry)
+	tflog.Info(ctx, "In Read method: Stop STATE logging")
+
+	state.HttpConfig = &httpConfigModel{
+		HttpEnabledState: types.StringValue(registry.HttpConfig.HttpEnabledState),
+	}
+
 	state.LogLevel = types.StringValue(registry.LogLevel)
-	//state.Region = types.StringValue(os.Getenv("CLEARBLADE_REGION"))
-	//state.Project = types.StringValue(os.Getenv("CLEARBLADE_PROJECT"))
+
+	state.MqttConfig = &mqttConfigModel{
+		MqttEnabledState: types.StringValue(registry.MqttConfig.MqttEnabledState),
+	}
+
+	state.StateNotificationConfig = &stateNotificationConfigModel{
+		PubsubTopicName: types.StringValue(registry.StateNotificationConfig.PubsubTopicName),
+	}
+
+	state.ID = types.StringValue(registry.Id)
+	//state.Name = types.StringValue(registry.Name)
 
 	// Set refreshed state
 	diags = resp.State.Set(ctx, &state)
@@ -398,6 +458,134 @@ func (r *deviceRegistryResource) Read(ctx context.Context, req resource.ReadRequ
 
 // Update updates the resource and sets the updated Terraform state on success.
 func (r *deviceRegistryResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan deviceRegistryResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Generate API request body from plan
+	event_notification_configs := []*iot.EventNotificationConfig{}
+	for _, v := range plan.EventNotificationConfigs {
+		event_notification_configs = append(event_notification_configs, &iot.EventNotificationConfig{
+			PubsubTopicName:  v.PubsubTopicName.ValueString(),
+			SubfolderMatches: v.SubfolderMatches.ValueString(),
+		})
+	}
+
+	credentials := []*iot.RegistryCredential{}
+	for _, v := range plan.Credentials {
+		credentials = append(credentials, &iot.RegistryCredential{
+			PublicKeyCertificate: &iot.PublicKeyCertificate{
+				Certificate: v.PublicKeyCertificate.Certificate.ValueString(),
+				Format:      v.PublicKeyCertificate.Format.ValueString(),
+				X509Details: &iot.X509CertificateDetails{
+					ExpiryTime:         v.PublicKeyCertificate.X509Details.X509CertificateDetail.ExpiryTime.ValueString(),
+					Issuer:             v.PublicKeyCertificate.X509Details.X509CertificateDetail.Issuer.ValueString(),
+					PublicKeyType:      v.PublicKeyCertificate.X509Details.X509CertificateDetail.PublicKeyType.ValueString(),
+					SignatureAlgorithm: v.PublicKeyCertificate.X509Details.X509CertificateDetail.SignatureAlgorithm.ValueString(),
+					StartTime:          v.PublicKeyCertificate.X509Details.X509CertificateDetail.StartTime.ValueString(),
+					Subject:            v.PublicKeyCertificate.X509Details.X509CertificateDetail.Subject.ValueString(),
+				},
+			},
+		})
+	}
+
+	stateNotificationConfig := &iot.StateNotificationConfig{
+		PubsubTopicName: plan.StateNotificationConfig.PubsubTopicName.ValueString(),
+	}
+
+	mqttConfig := &iot.MqttConfig{
+		MqttEnabledState: plan.MqttConfig.MqttEnabledState.ValueString(),
+	}
+
+	httpConfig := &iot.HttpConfig{
+		HttpEnabledState: plan.HttpConfig.HttpEnabledState.ValueString(),
+	}
+
+	tflog.Info(ctx, "In Create method: Start UPDATE logging")
+	ctx = tflog.SetField(ctx, "clearblade_provider_credentials", credentials)
+	tflog.Info(ctx, "In Create method: Stop UPDATE logging")
+
+	updateRequestPayload := iot.DeviceRegistry{
+		EventNotificationConfigs: event_notification_configs,
+		Credentials:              credentials,
+		Id:                       plan.ID.ValueString(),
+		Name:                     plan.Name.ValueString(),
+		StateNotificationConfig:  stateNotificationConfig,
+		MqttConfig:               mqttConfig,
+		HttpConfig:               httpConfig,
+		LogLevel:                 plan.LogLevel.ValueString(),
+	}
+
+	//parent := fmt.Sprintf("projects/%s/locations/%s", os.Getenv("CLEARBLADE_PROJECT"), os.Getenv("CLEARBLADE_REGION"))
+	parent := fmt.Sprintf("projects/%s/locations/%s/registries/%s", os.Getenv("CLEARBLADE_PROJECT"), os.Getenv("CLEARBLADE_REGION"), plan.ID.ValueString())
+
+	// Update an existing registry
+	_, err := r.client.Projects.Locations.Registries.
+		Patch(parent, &updateRequestPayload).
+		UpdateMask(`httpConfig.http_enabled_state,logLevel,mqttConfig.mqtt_enabled_state,stateNotificationConfig.pubsub_topic_name,credentials,eventNotificationConfigs`).
+		Do()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error updating a device registry",
+			"Could not update device registry, unexpected error: "+err.Error(),
+		)
+		return
+	}
+
+	// Fetch updated registry
+	registry, err := r.client.Projects.Locations.Registries.Get(parent).Do()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Reading ClearBlade IoT Core Registry",
+			"Could not read ClearBlade IoT Core registry ID "+plan.Name.ValueString()+": "+err.Error(),
+		)
+		return
+	}
+
+	// Update resource state
+	plan.Credentials = []credentialsModel{}
+	for _, credential := range registry.Credentials {
+		plan.Credentials = append(plan.Credentials, credentialsModel{
+			PublicKeyCertificate: publicKeyCertificateModel{
+				Format:      types.StringValue(credential.PublicKeyCertificate.Format),
+				Certificate: types.StringValue(credential.PublicKeyCertificate.Certificate),
+				X509Details: x509DetailsModel{
+					X509CertificateDetail: x509CertificateDetailModel{
+						Issuer:             types.StringValue(credential.PublicKeyCertificate.X509Details.Issuer),
+						Subject:            types.StringValue(credential.PublicKeyCertificate.X509Details.Subject),
+						StartTime:          types.StringValue(credential.PublicKeyCertificate.X509Details.StartTime),
+						ExpiryTime:         types.StringValue(credential.PublicKeyCertificate.X509Details.ExpiryTime),
+						SignatureAlgorithm: types.StringValue(credential.PublicKeyCertificate.X509Details.SignatureAlgorithm),
+						PublicKeyType:      types.StringValue(credential.PublicKeyCertificate.X509Details.PublicKeyType),
+					},
+				},
+			},
+		})
+	}
+
+	plan.EventNotificationConfigs = []eventNotificationConfigsModel{}
+	for _, eventNotificationConfig := range registry.EventNotificationConfigs {
+		plan.EventNotificationConfigs = append(plan.EventNotificationConfigs, eventNotificationConfigsModel{
+			PubsubTopicName:  types.StringValue(eventNotificationConfig.PubsubTopicName),
+			SubfolderMatches: types.StringValue(eventNotificationConfig.SubfolderMatches),
+		})
+	}
+
+	plan.HttpConfig.HttpEnabledState = types.StringValue(registry.HttpConfig.HttpEnabledState)
+	plan.MqttConfig.MqttEnabledState = types.StringValue(registry.MqttConfig.MqttEnabledState)
+	plan.StateNotificationConfig.PubsubTopicName = types.StringValue(registry.StateNotificationConfig.PubsubTopicName)
+	plan.LogLevel = types.StringValue(registry.LogLevel)
+	//plan.Name = types.StringValue(registry.Name)
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
 }
 
 // Delete deletes the resource and removes the Terraform state on success.
